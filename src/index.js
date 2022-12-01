3d25e8f91fdc133b8b3e55a6101fbd7be46ea7f5
asadbekded <asadbek171@gmail.com>
2022-12-01 15:03:08 +0500

    Add clear error

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/styles/index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-=======
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
->>>>>>> c4ebdc9afda73d64315fc08dfb956d4865b9e628
 );
