f32c5a8e2e72db8f8085cab1d6aeba5185a1a4d8
asadbekded <asadbek171@gmail.com>
2022-12-02 23:29:26 +0500

    Add change page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+// import { DishCard } from "./components/DishCard/DishCard";
+// import { Statistika } from "./components/Statistika/Statistika";
+// import { SettingsNested } from "./components/SettingsNested/SettingsNested";
+
+// import { DashboardHeader } from "./components/DashboardHeader/DashboardHeader";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Start goo</h1>
+    <div className="container">
+      {/* Dishcard componenta => <DishCard/> */}
+      {/*Statistica componenta  <Statistika/> */}
+      {/*Settings Nested components <SettingsNested/> */}
+      {/*Dashboard header <DashboardHeader/> */}
     </div>
   );
 };
---
+++ src/components/DishCard/DishCard.js
@@ -0,0 +1,59 @@
+import './DishCard.css';
+import Pizza from '../../assets/images/Image 1.png'
+
+export const DishCard = () => {
+   return (
+    <div>
+      <ul className='dish-list'>
+         <li className='dish-item'>
+            <img className='dish-img' src={Pizza} alt='' width={132} height={132}/>
+            <div className='dish-box'>
+              <h3 className='dish-title'>Spicy seasoned seafood noodles</h3>
+              <span className='dish-cur'>$ 2.29</span>
+              <p className='dish-text'>20 Bowls available</p>
+            </div>
+         </li>
+         <li className='dish-item'>
+            <img className='dish-img' src={Pizza} alt='' width={132} height={132}/>
+            <div className='dish-box'>
+              <h3 className='dish-title'>Spicy seasoned seafood noodles</h3>
+              <span className='dish-cur'>$ 2.29</span>
+              <p className='dish-text'>20 Bowls available</p>
+            </div>
+         </li>
+         <li className='dish-item'>
+            <img className='dish-img' src={Pizza} alt='' width={132} height={132}/>
+            <div className='dish-box'>
+              <h3 className='dish-title'>Spicy seasoned seafood noodles</h3>
+              <span className='dish-cur'>$ 2.29</span>
+              <p className='dish-text'>20 Bowls available</p>
+            </div>
+         </li>
+         <li className='dish-item'>
+            <img className='dish-img' src={Pizza} alt='' width={132} height={132}/>
+            <div className='dish-box'>
+              <h3 className='dish-title'>Spicy seasoned seafood noodles</h3>
+              <span className='dish-cur'>$ 2.29</span>
+              <p className='dish-text'>20 Bowls available</p>
+            </div>
+         </li>
+         <li className='dish-item'>
+            <img className='dish-img' src={Pizza} alt='' width={132} height={132}/>
+            <div className='dish-box'>
+              <h3 className='dish-title'>Spicy seasoned seafood noodles</h3>
+              <span className='dish-cur'>$ 2.29</span>
+              <p className='dish-text'>20 Bowls available</p>
+            </div>
+         </li>
+         <li className='dish-item'>
+            <img className='dish-img' src={Pizza} alt='' width={132} height={132}/>
+            <div className='dish-box'>
+              <h3 className='dish-title'>Spicy seasoned seafood noodles</h3>
+              <span className='dish-cur'>$ 2.29</span>
+              <p className='dish-text'>20 Bowls available</p>
+            </div>
+         </li>
+      </ul>
+    </div>
+  )
+}
