3f060935dfb74455d314f71f3a47735250e0bd28
asadbekded <easadbek171@gmail.com>
2022-12-25 13:14:10 +0500

    Add new component

--- src/components/SelectedDishes/SelectedDishes.js
+++ src/components/SelectedDishes/SelectedDishes.js
@@ -1,33 +1,34 @@
 import { Input } from '../Input/Input';
 import './selectedDishes.css';
 import Pizzaa from '../../assets/images/food4.svg';
+import Del from '../../assets/images/Trash.svg';
 
 export const SelectedDishes = () => {
   return (
     <>
-     <form className='selectedd-content'>
+     <div className='selectedd-content'>
       <ul className='selectedd-list'>
          <li className='selectedd-item'>
             <div>
                <div className='spice-box'>
                   <img src={Pizzaa} alt='pizza img' width={40} height={40}/>
-                  <div>
-                     <p>Spicy seasoned sea...</p>
-                     <ins>$ 2.29</ins>
+                  <div className='fill-box'>
+                     <p className='spice-text'>Spicy seasoned sea...</p>
+                     <ins className='spice-ins'>$ 2.29</ins>
                   </div>
                   <div>
-                    <button>1</button>
-                    <ins>$ 4,58</ins>
+                    <button className='spice-num'>1</button>
+                    <ins className='spice-price'>$ 4,58</ins>
                   </div>
                </div>
-               <div>
-                 <Input type='text' placeholder='Please, just a little bit spicy only.'/>
-                 <button></button>
+               <div className='spicehero-box'>
+                 <Input className="spice-inp" type='text' placeholder='Please, just a little bit spicy only.'/>
+                 <button className='spicedel-btn'><img src={Del} alt='delete img' width={20} height={20}/></button>
                </div>
             </div>
          </li>
       </ul>
-    </form>
+    </div>
    </>
   )
 }
