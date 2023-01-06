1f6be9d243a2d72ca2a322d2755caa5139720e79
asadbekded <easadbek171@gmail.com>
2023-01-06 15:55:20 +0500

    Add new change pages

--- src/components/HotDishes/HotDishes.js
+++ src/components/HotDishes/HotDishes.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
-// import { Watch } from 'react-loader-spinner';
-import { AddNewDishes } from '../AddNewDishes';
-import { DishCard } from '../DishCard';
+// import { AddNewDishes } from '../AddNewDishes';
+// import { DishCard } from '../DishCard';
 import './HotDishes.css';
+// import { Watch } from 'react-loader-spinner';
+
 
 export const HotDishes = () => {
-   const [form, setForm] = useState(false);
+   // const [form, setForm] = useState(false);
    const [hots, setHots] = useState([]);
 
    useEffect(() => {
@@ -15,52 +16,49 @@ export const HotDishes = () => {
          .get('http://localhost:5000/food/1')
          .then(res => setHots(res.data))
          .catch(err => console.log(err))
-   }, [hots])
+   }, [])
 
    return (
       <>
          {
             hots.length ? (
-               <DishCard>
-                  <li className='add__item'>
-                     <button onClick={() => setForm(true)} className='add__btn'>Add Dishes</button>
-                     {
-                        form && (
-                           <AddNewDishes form={form} setForm={setForm} />
-                        )
-                     }
-                  </li>
+               <ul className='dish-list'>
+                  {/* <li className='add__item'>
+                      <button onClick={() => setForm(true)} className='add__btn'>Add Dishes</button>
+                      {
+                         form && (
+                            <AddNewDishes form={form} setForm={setForm} />
+                         )
+                      }
+                   </li> */}
+
                   {
                      hots.map(el => (
-                        <li className='dishes__item'>
-                           <div className='dishes__box'>
-                              <img className='dishes__img' src={`http://localhost:5000/${el.image}`} width='127' height='127' alt="" />
-                              <h4 className='dishes__title'>{el.name}</h4>
-                              <div className='dishes__cost'>
-                                 <span className='dishes__price'>${el.price}</span>
-                                 <span className='dishes__bowl'>{el.bowls}</span>
-                              </div>
-                           </div>
-                           <div className='dishes__edit'>
-                              <button className='dishes__editBtn'>Edit dish</button>
+                        <li className='dish-item'>
+                           <img className='dish-img' src={`http://localhost:5000/${el.image}`} alt='img' width={132} height={132} />
+                           <div className='dish-box'>
+                           <h3 className='dish-title'>{el.name}</h3>
+                              <span className='dish-cur'>{el.price}</span>
+                              <p className='dish-text'>{el.bowls}</p>
                            </div>
                         </li>
                      ))
                   }
-               </DishCard>
+               </ul>
+
             ) : (
                <div className='laoding-box'>
-                  {/* <Watch
-                     className="loading"
-                     height="80"
-                     width="80"
-                     radius="48"
-                     color="#EA7C69FF"
-                     ariaLabel="watch-loading"
-                     wrapperStyle={{}}
-                     wrapperClassName=""
-                     visible={true}
-                  /> */}
+                      {/* <Watch
+                      className="loading"
+                      height="80"
+                      width="80"
+                      radius="48"
+                      color="#EA7C69FF"
+                      ariaLabel="watch-loading"
+                      wrapperStyle={{}}
+                      wrapperClassName=""
+                      visible={true}
+                   /> */}
                      <h2>Loading...</h2>
                   <div />
                </div>
